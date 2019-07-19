3ef4ea66baca18645bb92eced98d140d1f7f8e99
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-19 10:39:41 -0500

    [update] configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,5 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "space-before-function-paren": ["error", {
-            "anonymous": "always",
-            "named": "never",
-            "asyncArrow": "always"
-        }]
     }
 };
