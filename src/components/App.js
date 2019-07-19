3595fb15eac8ca9a3b9493d5c0b9d3fcc617da40
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-19 15:45:57 -0500

    [add] App component behavior

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,24 @@ import ButtonPanel from './ButtonPanel'
 import calculate from '../logic/calculate'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  handleClick (buttonName) {
+    this.setState(calculate(this.state, buttonName))
+  }
+
   render () {
     return (
       <div id='calculator'>
-        <Display />
-        <ButtonPanel />
+        <Display result={this.state.total} />
+        <ButtonPanel clickHandler={() => this.handleClick()} />
       </div>
     )
   }
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,12 +2,11 @@ import operate from './operate'
 
 const OPERATIONS_LIST = ['➗', '❌', '➖', '➕', '%', '+/-']
 
-function calculate (calculator, buttonName) {
-  const { total, next, operation } = calculator
+function calculate ({ total, next, operation }, buttonName) {
   if (OPERATIONS_LIST.includes(buttonName)) {
-    calculator.total = operate(total, next, buttonName)
+    total = operate(total, next, buttonName)
   }
-  return calculator
+  return { total, next, operation }
 }
 
 export default calculate
